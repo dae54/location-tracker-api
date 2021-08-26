2b5a1445bdc21c6a27a5517ebb7dcab80f99b8a1
dae54 <danielernest1.05@gmail.com>
2021-08-26 13:08:25 +0300

    add chat resource to routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const UserRoutes = require("./resources/Users/users.routes");
 const QuestionRoutes = require("./resources/question/question.routes");
+const ChatRoutes = require("./resources/chat/chat.routes");
 
 module.exports = (app) => {
   app.use("/api/v1/users", UserRoutes);
   app.use("/api/v1/questions", QuestionRoutes);
+  app.use("/api/v1/chat", ChatRoutes);
 
   app.use("/", (req, res) => res.redirect("http://apihomepage"));
 };
