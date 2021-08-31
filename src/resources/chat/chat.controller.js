b3d37d977c13a0647e20664b6bc296c1fd3a9fc8
dae54 <danielernest1.05@gmail.com>
2021-08-31 13:50:38 +0300

    get latest messages

--- src/resources/chat/chat.controller.js
+++ src/resources/chat/chat.controller.js
@@ -1,6 +1,6 @@
 // Customize, add new resources at your confort
 const Chat = require("./chat.modal");
-
+const mongoose = require('mongoose')
 module.exports = {
     getOne: async (req, res, next) => {
         // GET: Read Single Resources from here
@@ -16,9 +16,33 @@ module.exports = {
                 res.status(500).json({ message: error.message })
             })
     },
+    getQuestionsLatestMessage: async (req, res) => {
+        await Chat.find({ questionId })
+            .then(response => res.status(200).json(response))
+            .catch(error => {
+                res.status(500).json({ message: error.message })
+            })
+    },
+    getQuestionsLatestMessage: async (req, res) => {
+        const { questionId } = req.params
+        const { lastMessageSentAt, currentUser } = req.query
+        // console.log(lastMessageSentAt)
+        // console.log(currentUser)
+
+        await Chat.find({ questionId, sentAt: { $gt: lastMessageSentAt }, sender: { $ne: mongoose.Types.ObjectId(currentUser) } })
+            .then(data => {
+                console.log(data)
+                res.status(200).json(data)
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({ message: error.message })
+            })
+    },
     create: async (req, res) => {
         // POST: Create Resources here
         await Chat.create(req.body)
+
             .then(response => res.status(200).json(response))
             .catch(error => {
                 res.status(500).json({ message: error.message })
--- src/resources/chat/chat.routes.js
+++ src/resources/chat/chat.routes.js
@@ -4,6 +4,7 @@ const router = new Router();
 const chatController = require("./chat.controller");
 const sessionMonitor = require("../../utils/sessionMonitor");
 
+router.get("/question/latest/:questionId", sessionMonitor, chatController.getQuestionsLatestMessage);
 router.get("/question/:questionId", sessionMonitor, chatController.getQuestionsThread);
 router.get("/:id", sessionMonitor, chatController.getOne);
 router.get("/", sessionMonitor, chatController.getAll);
