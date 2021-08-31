d7f031e73a9982aff8a26d34ad8c2c6556ae6c72
dae54 <danielernest1.05@gmail.com>
2021-08-31 12:37:31 +0300

    qn count

--- src/resources/question/question.routes.js
+++ src/resources/question/question.routes.js
@@ -6,6 +6,7 @@ const sessionMonitor = require("../../utils/sessionMonitor");
 
 router.get("/user/:userID", sessionMonitor, questionController.getUserQuestions);
 router.get("/tutor/:userID", sessionMonitor, questionController.getQuestionsAnsweredByTutorID);
+router.get("/count/:userID", sessionMonitor, questionController.getQuestionsCount);
 router.get("/:id", sessionMonitor, questionController.getOne);
 router.get("/", sessionMonitor, questionController.getAll);
 
