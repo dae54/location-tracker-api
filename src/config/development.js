d553342094850d27f9c5c3d8454e7c6d56d1955c
dae54 <46072940+dae54@users.noreply.github.com>
2021-09-07 10:26:42 +0300

    remove mongodb remote liink from dev env

--- src/config/development.js
+++ src/config/development.js
@@ -1,5 +1,4 @@
 module.exports = {
   DB_URL: "mongodb://localhost:27017/location-aware",
-  // DB_URL: "mongodb://dae54:dhbFF7ybiinm@cluster0-shard-00-00.y7wir.mongodb.net:27017,cluster0-shard-00-01.y7wir.mongodb.net:27017,cluster0-shard-00-02.y7wir.mongodb.net:27017/location_aware?ssl=true&replicaSet=atlas-wqus6n-shard-0&authSource=admin&retryWrites=true&w=majority",
   PORT: 5400,
 };
